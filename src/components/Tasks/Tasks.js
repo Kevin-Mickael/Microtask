3f56498e14375dc6bae8be9cf4aa0d4360ca4e8b
Lawrence Lo <lolung@gmail.com>
2022-06-07 22:03:17 -0400

    Improve tasks sorting and add error handling

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -10,9 +10,11 @@ const axios = require("axios");
 const Tasks = () => {
   const [tasks, setTasks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getTasksHandler = useCallback(() => {
     setIsLoading(true);
+    setError(null);
 
     axios
       .get(`${config.baseUrl}/tasks`, {
@@ -38,15 +40,17 @@ const Tasks = () => {
           });
         }
 
-        loadedTasks.reverse();
+        // Sort by date in reverse order
+        loadedTasks.reverse((a, b) => {
+          return new Date(a.date) - new Date(b.date);
+        });
 
         setTasks(loadedTasks);
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error.message);
+        setError(error.message);
       });
-
-      
 
     setIsLoading(false);
   }, []);
@@ -56,30 +60,33 @@ const Tasks = () => {
   }, [getTasksHandler]);
 
   const addTaskHandler = (task) => {
-
     axios
-    .post(`${config.baseUrl}/tasks`, task, {
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": localStorage.getItem("token"),
-      },
-    }).then((response) => {
-      console.log(response)
+      .post(`${config.baseUrl}/tasks`, task, {
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        console.log(response);
 
-      setTasks((prevTasks) => {
-        return [{
-          id: response.data._id,
-          date: response.data.date,
-          type: response.data.type,
-          minute: response.data.minute,
-          second: response.data.second,
-          count: response.data.count,
-        }, ...prevTasks];
+        setTasks((prevTasks) => {
+          return [
+            {
+              id: response.data._id,
+              date: response.data.date,
+              type: response.data.type,
+              minute: response.data.minute,
+              second: response.data.second,
+              count: response.data.count,
+            },
+            ...prevTasks,
+          ];
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    })
-    .catch((error) => {
-      console.log(error)
-    });
 
     // setTasks((prevTasks) => {
     //   return [task, ...prevTasks];
@@ -93,22 +100,38 @@ const Tasks = () => {
     });
   };
 
+  let content = (
+    <Typography variant="h2" align="center">
+      No tasks found.
+    </Typography>
+  );
+
+  if (tasks.length > 0) {
+    content = <TasksList items={tasks} onDeleteItem={deleteItemHandler} />;
+  }
+
+  if (error) {
+    content = (
+      <Typography variant="h2" align="center">
+        {error}
+      </Typography>
+    );
+  }
+
+  if (isLoading) {
+    content = (
+      <Typography variant="h2" align="center">
+        Loading...
+      </Typography>
+    );
+  }
+
   return (
     <div>
       <NewTask onAddTask={addTaskHandler} />
       <TasksSummary items={tasks} />
 
-      {isLoading ? (
-        <Typography variant="h2" align="center">
-          Loading...
-        </Typography>
-      ) : tasks.length === 0 ? (
-        <Typography variant="h2" align="center">
-          No tasks found.
-        </Typography>
-      ) : (
-        <TasksList items={tasks} onDeleteItem={deleteItemHandler} />
-      )}
+      {content}
     </div>
   );
 };
