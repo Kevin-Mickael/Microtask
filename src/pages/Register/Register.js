aff28bc859c03ff9089d4bf13461045fba003782
Lawrence Lo <lolung@gmail.com>
2022-06-15 00:15:14 -0400

    Change registration form design

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import config from "../../config";
+
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+
+const axios = require("axios");
 
 const Register = () => {
   const {
@@ -11,112 +21,130 @@ const Register = () => {
   } = useForm();
 
   const [message, setMessage] = useState();
+  const history = useHistory();
 
-  const onSubmit = (data, e) => {
+  const submitHandler = (data, e) => {
     setMessage({
       data: "Registration is in progress...",
     });
 
-    fetch(`${config.baseUrl}/user/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const hasError = "error" in data && data.error != null;
+    axios
+      .post(`${config.baseUrl}/user/register`, data)
+      .then((response) => {
+        // const data = response.data.data;
+
         setMessage({
-          data: hasError ? data.error : "Registered successfully.",
+          data: "Registered successfully, redirecting to the login page...",
+          severity: "success",
         });
 
-        !hasError && e.target.reset();
+        setTimeout(() => {
+          history.push("/login");
+        }, 3000);
+
+        e.target.reset();
+      })
+      .catch((error) => {
+        setMessage({
+          data: error.response.data.error,
+          severity: "error",
+        });
       });
   };
 
   return (
-    <div>
-      <div>
-        {message && (
-          <div role="alert">
-            {message.data}
-            <span aria-hidden="true" onClick={() => setMessage(null)}>
-              &times;
-            </span>
-          </div>
-        )}
-        <fieldset>
-          <legend>Registration Form</legend>
-          <form onSubmit={handleSubmit(onSubmit)} noValidate>
-            <div>
-              <label htmlFor="username">Username: </label>
-              <input
-                id="username"
-                type="text"
-                placeholder="Username"
-                {...register("username", {
-                  required: {
-                    value: true,
-                    message: "Please enter a username.",
-                  },
-                  minLength: {
-                    value: 3,
-                    message: "Username should contain at least 3 characters.",
-                  },
-                  maxLength: {
-                    value: 30,
-                    message: "Username should have a maximum of 30 characters.",
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9_-]{3,30}$/,
-                    message:
-                      "Username should contain letters, numbers, underscore, and dash only.",
-                  },
-                })}
-              />
-              {errors.username && <p>{errors.username.message}</p>}
-            </div>
-            <div>
-              <label htmlFor="password">Password: </label>
-              <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Please enter a password.",
-                  },
-                  minLength: {
-                    value: 6,
-                    message: "Password should contain at least 6 characters.",
-                  },
-                  maxLength: {
-                    value: 128,
-                    message:
-                      "Password should have a maximum of 128 characters.",
-                  },
-                  pattern: {
-                    value:
-                      /^\S*(?=\S{6,128})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
-                    message:
-                      "Password should contain at least 1 upper case English letter, 1 lower case English letter, and 1 special character.",
-                  },
-                })}
-              />
-              {errors.password && <p>{errors.password.message}</p>}
-            </div>
-            <div>
-              <button type="submit">Submit</button>
-              <button>
-                <Link to="/login">Cancel</Link>
-              </button>
-            </div>
-          </form>
-        </fieldset>
-      </div>
-    </div>
+    <Container maxWidth="sm">
+      {message && <Alert severity={message.severity}>{message.data}</Alert>}
+
+      <Box
+        component="form"
+        onSubmit={handleSubmit(submitHandler)}
+        noValidate
+        autoComplete="off"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h1" component="div">
+          User Registration
+        </Typography>
+        <TextField
+          fullWidth
+          sx={{ mb: 3 }}
+          error={errors.username}
+          helperText={errors.username ? errors.username.message : ""}
+          variant="filled"
+          label="Username"
+          id="username"
+          type="text"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Please enter a username.",
+            },
+            minLength: {
+              value: 3,
+              message: "Username should contain at least 3 characters.",
+            },
+            maxLength: {
+              value: 30,
+              message: "Username should have a maximum of 30 characters.",
+            },
+            pattern: {
+              value: /^[a-zA-Z0-9_-]{3,30}$/,
+              message:
+                "Username should contain letters, numbers, underscore, and dash only.",
+            },
+          })}
+        />
+        <TextField
+          fullWidth
+          sx={{ mb: 3 }}
+          error={errors.password}
+          helperText={errors.password ? errors.password.message : ""}
+          variant="filled"
+          label="Password"
+          id="password"
+          type="password"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "Please enter a password.",
+            },
+            minLength: {
+              value: 6,
+              message: "Password should contain at least 6 characters.",
+            },
+            maxLength: {
+              value: 128,
+              message: "Password should have a maximum of 128 characters.",
+            },
+            pattern: {
+              value:
+                /^\S*(?=\S{6,128})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
+              message:
+                "Password should contain at least 1 upper case English letter, 1 lower case English letter, and 1 special character.",
+            },
+          })}
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          type="submit"
+          sx={{ borderRadius: 5, mb: 5 }}
+        >
+          Submit
+        </Button>
+        <Typography variant="h6" component="div">
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Cancel
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
