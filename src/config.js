7d8ee4fcb60c41b0d023a08de99aeff5de0a4259
Lawrence Lo <lolung@gmail.com>
2022-06-19 02:23:44 -0400

    Fix typo in baseUrl

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
   // baseUrl: "http://localhost:3001/api",
-  baseUrl: "https://microtask-counter-backend.herokuapp.com//api",
+  baseUrl: "https://microtask-counter-backend.herokuapp.com/api",
 };
 export default config;
